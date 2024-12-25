de8041f808b5cfd720c857dcdf647fc0ab7098e4
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-12-25 20:57:23 +0600

    Update navbar.blade.php

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="headerMain">
             <a href="{{route('home')}}" class="headerLogo">
-                <img src="{{asset($site_info->logo)}}" alt="Logo">
+                <img class="m-auto" src="{{asset($site_info->logo)}}" alt="Logo">
                 {{-- <h2 class="logotitle">{{$site_info->title}}</h2> --}}
             </a>
         </div>
